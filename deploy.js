acb1e03797e6542ee6c404024c4f18edd0b56171
Joel <Lianb1@nku.edu>
2023-12-17 15:56:06 -0500

    update

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { abi, bytecode } = require('../my-ticket-react/compile');
+const { abi, bytecode } = require('./compile');
 
 /*const provider = new HDWalletProvider(
   'crucial only supply tattoo erosion hen ancient edit guitar right cargo cactus',
