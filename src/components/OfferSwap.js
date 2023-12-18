9a24e38f7a86e3e04ec1b2afb89b9c055e626934
Joel <Lianb1@nku.edu>
2023-12-18 13:59:49 -0500

    added asset folder

--- src/components/OfferSwap.js
+++ src/components/OfferSwap.js
@@ -3,18 +3,26 @@ import './OfferSwap.css';
 import web3 from '../web3';
 import myTicket from '../myTicket';
 
+// Mock implementation of convertTicketNumberToAddress
+const convertTicketNumberToAddress = (ticketNumber) => {
+  // Convert the ticket number to a valid Ethereum address
+  // Mock Implementation - replace later
+  return '0x' + ticketNumber.padStart(40, '0');
+};
+
 function OfferSwap() {
-  const [offerAddress, setOfferAddress] = useState('');
+  const [ticketNumber, setTicketNumber] = useState('');
 
   const onOffer = async () => {
     const accounts = await web3.eth.getAccounts();
+    const offerAddress = convertTicketNumberToAddress(ticketNumber);
     await myTicket.methods.offerSwap(offerAddress).send({ from: accounts[0] });
     alert("Offer sent!");
   };
 
   return (
     <div className="offer-swap">
-      <input type="text" placeholder="Enter ticket number" value={offerAddress} onChange={e => setOfferAddress(e.target.value)} />
+      <input type="text" placeholder="Enter ticket number" value={ticketNumber} onChange={e => setTicketNumber(e.target.value)} />
       <button onClick={onOffer}>Offer Swap</button>
     </div>
   );
